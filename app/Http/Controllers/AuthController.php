0537de9a182f4b4c1cdf7fe562576b8be91d2e52
aryspd <arya.sukma.420@gmail.com>
2023-07-29 02:51:31 +0700

    as

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use TypeError;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
-        return view('login');
-    }
+    public function login(Request $request)
+        {
+            return view('login');
+        }
 
+        public function register()
+        {
+            return view('register');
+        }
 }
