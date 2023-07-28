e401140cdc5c38f774dae7859a4cdb45b9dbe97b
212Project <212fathany.azrul@gmail.com>
2023-07-28 21:54:34 +0700

    fix sxhema table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone', 14)->nullable();
+            $table->text('alamat');
+            $table->string('status')->default('inactive');
             $table->timestamps();
         });
     }
