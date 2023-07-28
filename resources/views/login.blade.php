28701ff4139fabf4744d9ec340396549755b33f4
aryspd <arya.sukma.420@gmail.com>
2023-07-29 01:55:23 +0700

    sa

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,8 +8,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 </head>
+
+<style>
+    .main {
+        height: 100vh;
+        box-sizing: border-box;
+    }
+
+    .login {
+        width: 500px;
+        border: solid 1px;
+    }
+</style>
+
 <body>
-    Hallo
+
+    <div class="main d-flax justify-content-center align-items-center">
+        <div class="login-box">
+            <form action="" method="post">
+                @csrf
+                <div>
+                    <input for="username" class="form-label">Username</label>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div>
+                    <label for="paswpprd">Passord</label>
+                    <input type="password" name="password" id="password">
+                </div>
+            </form>
+        </div>
+    
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
