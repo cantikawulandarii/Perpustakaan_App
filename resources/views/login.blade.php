102688ea24f4874f43efd636b8ebb2479079a070
aryspd <arya.sukma.420@gmail.com>
2023-07-29 02:22:27 +0700

    sa

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,26 +15,33 @@
         box-sizing: border-box;
     }
 
-    .login {
+    .login-box {
         width: 500px;
         border: solid 1px;
+        padding: 30px
+    }
+
+    form div {
+        margin-bottom: 15px
     }
 </style>
 
 <body>
 
-    <div class="main d-flax justify-content-center align-items-center">
+    <div class="main d-flex justify-content-center align-items-center">
         <div class="login-box">
             <form action="" method="post">
                 @csrf
                 <div>
-                    <input for="username" class="form-label">Username</label>
-                    <input type="text" name="username" id="username">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="username" id="username" class="form-control">
                 </div>
                 <div>
-                    <label for="paswpprd">Passord</label>
-                    <input type="password" name="password" id="password">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password" class="form-control">
                 </div>
+                <div>
+                    <button type="submit" class="btn btn-primary form-control">Login</button>
             </form>
         </div>
     
