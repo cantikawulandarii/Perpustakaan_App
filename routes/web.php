9ec161fc0cd632f3062e2aa29033c30111d63221
aryspd <arya.sukma.420@gmail.com>
2023-07-29 03:43:35 +0700

    q

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-Route::get('login', [AuthController::class, 'login']);
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'authenticasting']);
 Route::get('register', [AuthController::class, 'register']);
