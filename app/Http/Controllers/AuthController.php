450a5302fbee822ac0ef850ab6a054fcf55202a9
aryspd <arya.sukma.420@gmail.com>
2023-07-29 10:11:14 +0700

    fitur login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use TypeError;
 
 class AuthController extends Controller
@@ -19,7 +21,27 @@ class AuthController extends Controller
 
         public function authentication(Request $request)
         {
-            dd('ini halaman authenticating');
+            $credentials = $request->validate([
+                'username' => ['required'],
+                'password' => ['required'],
+            ]);
+
+            // cek apakah login valid
+            // cek apakah user status = active
+            if (Auth::attempt($credentials)) {
+                //cek apakah user status = active
+                if(Auth::user()->status != 'active'){
+                    Session::flash('status', 'failde');
+                    Session::flash('message', 'Your account is not active yet. please contact admin!');
+                    return redirect('/login');
+                }
+                //$request->session()->regenerate();
+                //return redirect()->intended('dashboard');
+            }
+
+                    Session::flash('status', 'failde');
+                    Session::flash('message', 'Login Invalid');
+                    return redirect('/login');
 
         }
 }
