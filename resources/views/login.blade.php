95254cd007f3fd1b49f9c58083f0c6918ff37442
aryspd <arya.sukma.420@gmail.com>
2023-07-29 10:47:19 +0700

    fitur login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -29,12 +29,17 @@
 <body>
 
     <div class="main d-flex justify-content-center align-items-center">
+        @if (session('status'))
+        <div class= "alert alert-danger">
+            {{ session("message")}}
+        </div>
+        @endif
         <div class="login-box">
             <form action="" method="post">
                 @csrf
                 <div>
                     <label for="username" class="form-label">Username</label>
-                    <input type="text" name="username" id="username" class="form-control"required>
+                    <input type="text" name="username" id="username" class="form-control" required>
                 </div>
                 <div>
                     <label for="password" class="form-label">Password</label>
