429e34600646190a968e849d1658b460ac086f3c
212Project <212fathany.azrul@gmail.com>
2023-07-29 21:32:53 +0700

    fix tampilan login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link href="{{ asset('css/stylehome.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/stylelogin.css') }}" rel="stylesheet">
 </head>
 
 <style>
