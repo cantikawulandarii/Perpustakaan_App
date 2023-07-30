df28351005f36eab6ff313bda5dfed2821610bd0
cantikawulandarii <cantikawulandari2017@gmail.com>
2023-07-31 02:24:46 +0700

    bug rentlog

--- app/Http/Controllers/RentLogController.php
+++ app/Http/Controllers/RentLogController.php
@@ -2,18 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Book;
-use App\Models\User;
-use App\Models\Category;
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
+class RentLogController extends Controller
 {
     public function index()
     {
-        $bookCount = Book::count();
-        $categoryCount = Category::count();
-        $userCount = User::count();
-        return view('dashboard', ['book_count' => $bookCount, 'category_count' => $categoryCount, 'user_count' => $userCount]);
+        return view('rentlog');
     }
 }
