5dde178fce408b69743950ad3473dafc9a29bec3
aryspd <arya.sukma.420@gmail.com>
2023-08-01 00:31:45 +0700

    login

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <title>Rental Buku | @yield('title')</title>
+    <title>Perpustakaan | @yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <div class="main d-flex flex-column justify-content">
         <nav class="navbar navbar-dark navbar-expand-lg bg-danger">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Rental Buku</a>
+                <a class="navbar-brand" href="#">Perpustakaan</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
