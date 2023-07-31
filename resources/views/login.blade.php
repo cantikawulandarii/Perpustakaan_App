ae0633a143ee4c94ac8f190c950e1517d25fa992
212Project <212fathany.azrul@gmail.com>
2023-07-31 21:16:05 +0700

    ubah login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -30,24 +30,36 @@
                 {{ session('message') }}
             </div>            
         @endif
-        <div class="login-box">
-            <form action="" method="POST">
-                @csrf
-                <div>
-                    <label for="username" class="form-label">Username</label>
-                    <input type="text" name="username" id="username" class="form-control" required>
-                </div>
-                <div>
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" name="password" id="password" class="form-control" required>
-                </div>
-                <div>
-                    <button type="submit" class="btn btn-primary form-control">Login</button>
-                </div>
-                <div class="text-center"> 
-                    Don't have account? <a href="register">Sign Up</a>
-                </div>
-            </form>
+        <div class="section">
+            <div class="container">
+                <div class="row full-height justify-content-center">
+                    <div class="col-12 text-center align-self-center py-5">
+                        <div class="section pb-5 pt-5 pt-sm-2 text-center">
+                            <div class="card-3d-wrap mx-auto">
+                                <div class="card-3d-wrapper">
+                                    <div class="card-front">
+                                        <div class="center-wrap">
+                                            <div class="section text-center">
+                                                <h4 class="mb-4 pb-3">Log In</h4>
+                                                <div class="form-group">
+                                                    <input type="username" name="username" class="form-style" placeholder="Your Email" id="username" autocomplete="off">
+                                                    <i class="input-icon uil uil-at"></i>
+                                                </div>	
+                                                <div class="form-group mt-2">
+                                                    <input type="password" name="password" class="form-style" placeholder="Your Password" id="password" autocomplete="off">
+                                                    <i class="input-icon uil uil-lock-alt"></i>
+                                                </div>
+                                                <a href="submit" class="btn mt-4">submit</a>
+                                                <p class="mb-0 mt-4 text-center"><a href="register" class="link">Register</a></p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+            </div>
         </div>
     </div>
 
